54c38926111e38b06dae71b90ce1c05bebf77ede
AjayaDeep <dejay.sth@gmail.com>
2024-04-02 07:38:12 +0545

    Add Mapster for object mapping

---
+++ UberEats.Api/Common/Mapping/AuthenticationMappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using Mapster;
+using UberEats.Application.Authentication.Common;
+using UberEats.Contracts.Authentication;
+
+namespace UberEats.Api.Common.Mapping
+{
+    public class AuthenticationMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<AuthenticationResult, AuthenticationResponse>()
+                .Map(dest => dest.Token, src => src.Token)
+                .Map(dest => dest , src => src.User);
+        }
+    }
+}
--- UberEats.Api/Controllers/AuthenticationController.cs
+++ UberEats.Api/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
 ﻿using ErrorOr;
+using MapsterMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using UberEats.Application.Authentication.Commands.Register;
@@ -14,31 +15,29 @@ namespace UberEats.Api.Controllers
     public class AuthenticationController : ApiController
     {
         private readonly ISender _mediator;
+        private readonly IMapper _mapper;
 
-        public AuthenticationController(ISender mediator)
+        public AuthenticationController(ISender mediator, IMapper mapper)
         {
             _mediator = mediator;
+            _mapper = mapper;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> Register(RegisterRequest request)
         {
-            var command = new RegisterCommand(
-                request.FirstName,
-                request.LastName,
-                request.Email,
-                request.Password);
+            var command = _mapper.Map<RegisterCommand>(request);
             ErrorOr<AuthenticationResult> authResult = await _mediator.Send(command);
            
             return authResult.Match(
-                authResult => Ok(MapAuthResult(authResult)),
+                authResult => Ok(_mapper.Map<AuthenticationResult>(authResult)),
                 errors => Problem(errors));
         }
 
         [HttpPost("login")]
         public async Task<IActionResult> Login(LoginRequest request)
         {
-            var query = new LoginQuery(request.Email, request.Password);
+            var query = _mapper.Map<LoginQuery>(request);
             var authResult = await _mediator.Send(query);
             if (authResult.IsError && authResult.FirstError == Errors.Authentication.InvalidCredentials)
             {
@@ -47,18 +46,8 @@ namespace UberEats.Api.Controllers
                     title: authResult.FirstError.Description);
             }
             return authResult.Match(
-                         authResult => Ok(MapAuthResult(authResult)),
+                         authResult => Ok(_mapper.Map<AuthenticationResult>(authResult)),
                          errors => Problem(errors));
-        }
-        private static AuthenticationResponse MapAuthResult(AuthenticationResult authResult)
-        {
-            return new AuthenticationResponse(
-            authResult.User.Id,
-            authResult.User.FirstName,
-            authResult.User.LastName,
-            authResult.User.Email,
-            authResult.Token
-            );
         }
     }
 }
