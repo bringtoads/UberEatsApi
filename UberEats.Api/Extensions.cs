ad43255e3f7d9f550a0105600bfb257d9a841334
AjayaDeep <dejay.sth@gmail.com>
2024-04-02 07:55:06 +0545

    Add Extensions in api for presentationlayer

---
+++ UberEats.Api/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Infrastructure;
+using UberEats.Api.Common.Errors;
+using UberEats.Api.Common.Mapping;
+
+namespace UberEats.Api
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddPresentationCore(this IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSingleton<ProblemDetailsFactory, UberEatsProblemDetailsFactory>();
+            services.AddMappingsCore();
+            return services;
+        }
+    }
+}
--- UberEats.Api/Program.cs
+++ UberEats.Api/Program.cs
@@ -1,5 +1,4 @@
-using Microsoft.AspNetCore.Mvc.Infrastructure;
-using UberEats.Api.Common.Errors;
+using UberEats.Api;
 using UberEats.Application;
 using UberEats.Infrastructure;
 
@@ -7,26 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 {
     // Add services to the container
     builder.Services
+        .AddPresentationCore()
         .AddApplicationCore()
         .AddInfrastructureCore(builder.Configuration);
 
-    builder.Services.AddControllers();
-
-    builder.Services.AddSingleton<ProblemDetailsFactory, UberEatsProblemDetailsFactory>();
-
-    //builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 }
 
 var app = builder.Build();
 {
-    // app.UseMiddleware<ErrorHandlingMiddleware>();
     app.UseExceptionHandler("/error");
-    //using minimal api instead of controller
-    //app.Map("/error",(HttpContext httpContext) =>
-    //{
-    //    Exception? exception = httpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
-    //    return Results.Problem();
-    //});
 
     app.UseHttpsRedirection();
     app.MapControllers();
